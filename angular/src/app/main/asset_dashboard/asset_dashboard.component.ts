3bb3aa677d27cdde0d754d467e8bcbe0f980015b
15520429uit <47861337+15520429uit@users.noreply.github.com>
2019-05-07 12:57:15 +0700

    7/5

--- angular/src/app/main/asset_dashboard/asset_dashboard.component.ts
+++ angular/src/app/main/asset_dashboard/asset_dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
-import { DonViServiceProxy, DonViDto, ListResultDtoOfDonViDto } from '@shared/service-proxies/service-proxies';
+import { DonViServiceProxy, DonViDto } from '@shared/service-proxies/service-proxies';
 
 @Component({
     templateUrl: './asset_dashboard.component.html',
@@ -13,12 +13,14 @@ export class AssetDashboardComponent extends AppComponentBase implements OnInit
 {
 	userName = '';
 	header_stats: AssetHeaderStats;
+	chart: PieChartData;
 	assets_state_chart: ProfitSharePieChart;
 
     constructor(injector: Injector, private _DonViService: DonViServiceProxy) 
     {
         super(injector);
         this.header_stats = new AssetHeaderStats();
+        this.chart = new PieChartData();
         this.assets_state_chart = new ProfitSharePieChart();
         this.userName = this.appSession.user.userName;
     }
@@ -29,23 +31,48 @@ export class AssetDashboardComponent extends AppComponentBase implements OnInit
     	{
         	this._DonViService.getDonVi("DonViChinh").subscribe( result => 
         	{
-                this.header_stats.Assets_Count = result[0].soLuongTaiSan;
-                this.header_stats.Assets_Active = result[0].taiSanSuDung;
-                this.header_stats.Assets_Storage = result[0].taiSanTrongKho;
-                this.header_stats.Assets_Disposed = result[0].taiSanHu;
+                this.get_display_data(result);
             });
         }
         else
         {
         	this._DonViService.getDonVi(this.userName).subscribe( result => 
         	{
-                this.header_stats.Assets_Count = result[0].soLuongTaiSan;
-                this.header_stats.Assets_Active = result[0].taiSanSuDung;
-                this.header_stats.Assets_Storage = result[0].taiSanTrongKho;
-                this.header_stats.Assets_Disposed = result[0].taiSanHu;
+                this.get_display_data(result);
             });
         }
     }
+
+    get_display_data(result): void
+    {
+    	this.header_stats.Assets_Count = result[0].soLuongTaiSan;
+        this.header_stats.Assets_Active = result[0].taiSanSuDung;
+        this.header_stats.Assets_Storage = result[0].taiSanTrongKho;
+        this.header_stats.Assets_Disposed = result[0].taiSanHu;
+
+        if(this.header_stats.Assets_Count > 0)
+        {
+        	let perimeter = 2* 3.14 * 100;
+        	this.chart.Perimeter = perimeter;
+        	this.chart.Assets_Active_Span = 0;
+        	this.chart.Assets_Storage_Span = 1 - (result[0].taiSanTrongKho/result[0].soLuongTaiSan) - (result[0].taiSanHu/result[0].soLuongTaiSan);
+        	this.chart.Assets_Disposed_Span = 1 - (result[0].taiSanHu/result[0].soLuongTaiSan);
+        	this.chart.Assets_Active_Span *= -perimeter;
+        	this.chart.Assets_Storage_Span *= -perimeter;
+        	this.chart.Assets_Disposed_Span *= -perimeter;
+        }
+    }
+
+}
+
+
+
+class PieChartData
+{
+	Perimeter = 0;
+    Assets_Active_Span = 0;
+    Assets_Storage_Span = 0;
+    Assets_Disposed_Span = 0;
 }
 
 class AssetHeaderStats
@@ -58,15 +85,6 @@ class AssetHeaderStats
     Assets_Storage_Counter = 0;
     Assets_Disposed = 0; 
     Assets_Disposed_Counter = 0;
-
-    /*init(totalProfit, newFeedbacks, newOrders, newUsers)
-    {
-        this.totalProfit = totalProfit;
-        this.newFeedbacks = newFeedbacks;
-        this.newOrders = newOrders;
-        this.newUsers = newUsers;
-        this.hideLoading();
-    }*/
 }
 
 
