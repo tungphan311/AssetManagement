fdb485fa8ce7101395886ec686533cc65baa8c67
dangquang9a <dangquang9a@gmail.com>
2019-05-14 15:35:44 +0700

    Add Contract

---
+++ aspnet-core/src/GWebsite.AbpZeroTemplate.Application.Share/Contracts/IContractAppService.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services.Dto;
+using GWebsite.AbpZeroTemplate.Application.Share.Contracts.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWebsite.AbpZeroTemplate.Application.Share.Contracts
+{
+    public interface IContractAppService
+    {
+        void CreateOrEditContract(ContractInput contractInput);
+        ContractInput GetContractForEdit(int id);
+        void DeleteContract(int id);
+        PagedResultDto<ContractDto> GetContracts(ContractFilter input);
+        ContractForViewDto GetContractForView(int id);
+    }
+}
