af1bd9d8afff08eb064070cb7224074ede1012d7
Thu Nguyen <hoaithu7604@gmail.com>
2019-05-25 17:27:56 +0900

    update bid's menu

--- angular/src/app/main/bid/create-or-edit-bid-modal.component.ts
+++ angular/src/app/main/bid/create-or-edit-bid-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, EventEmitter, Injector, Output, ViewChild } from
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { ModalDirective } from 'ngx-bootstrap';
 import { BidServiceProxy, BidInput } from '@shared/service-proxies/service-proxies';
-
+import * as moment from 'moment';
 
 @Component({
     selector: 'createOrEditBidModal',
@@ -39,6 +39,33 @@ export class CreateOrEditBidModalComponent extends AppComponentBase {
 
         this._bidService.getBidForEdit(bidId).subscribe(result => {
             this.bid = result;
+            this.bid.biddingForm = this.bid.biddingForm || 'Đấu thầu';
+            var moment = require('moment');     
+            //beginDay 
+            if (result.beginDay!=null)
+            {
+                var _beginDay = moment(result.beginDay);
+                var tz = _beginDay.utcOffset(); 
+                _beginDay.add(tz, 'm');
+                this.bid.beginDay = _beginDay.format('YYYY-MM-DD');
+            }
+            //startReceivingRecords
+            if (result.startReceivingRecords!=null)
+            {
+            var _startReceivingRecords = moment(result.startReceivingRecords);
+            var tz = _startReceivingRecords.utcOffset(); 
+            _startReceivingRecords.add(tz, 'm');
+            this.bid.startReceivingRecords = _startReceivingRecords.format('YYYY-MM-DD');
+            }
+            //endReceivingRecords 
+            if (result.endReceivingRecords!=null)
+            {
+            var _endReceivingRecords = moment(result.endReceivingRecords);
+            var tz = _endReceivingRecords.utcOffset(); 
+            _endReceivingRecords.add(tz, 'm');
+            this.bid.endReceivingRecords = _endReceivingRecords.format('YYYY-MM-DD');
+            }
+            //--------------------
             this.modal.show();
 
         })
@@ -48,6 +75,8 @@ export class CreateOrEditBidModalComponent extends AppComponentBase {
         let input = this.bid;
         var moment = require('moment');
         input.beginDay = moment(input.beginDay);
+        input.startReceivingRecords = moment(input.startReceivingRecords);
+        input.endReceivingRecords = moment(input.endReceivingRecords);
         this.saving = true;
         this._bidService.createOrEditBid(input).subscribe(result => {
             this.notify.info(this.l('SavedSuccessfully'));
